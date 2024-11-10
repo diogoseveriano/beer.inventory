ff18162260780b5fc4709f84fd9f85e024f3bb64
Diogo Severiano <diogo.severiano@outlook.com>
2024-11-10 03:30:23 +0000

    change login request mode

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,12 +1,11 @@
 package controller;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import records.LoginRequest;
 import service.UserService;
 
 @Path(value = "api/login")
@@ -16,9 +15,10 @@ public class LoginController {
     UserService userService;
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response login(@QueryParam("email") String email, @QueryParam("password") String password) {
-        return Response.ok(userService.login(email, password)).build();
+    public Response login(@RequestBody LoginRequest loginRequest) {
+        return Response.ok(userService.login(loginRequest.email(), loginRequest.password())).build();
     }
 
     @POST
